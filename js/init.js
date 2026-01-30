1f5974eebdae40f9b5c1944f4b4f3d7dc54ac9e5
Khizar Imtiaz <khizar.imtiaz@gmail.com>
2026-01-31 01:44:17 +0500

    GA 4 Added

--- js/init.js
+++ js/init.js
@@ -3,6 +3,34 @@
     ? "https://khizarooo.github.io/mysite/"
     : "file:///D:/khizooo/WEBSITE/static-website/mysite/";
 
+// ===============================
+// Google Analytics 4 (GA4)
+// ===============================
+(function() {
+  // Only load GA4 on production (GitHub Pages)
+  if (window.location.hostname === "khizarooo.github.io") {
+    // Load gtag.js script
+    const gtagScript = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=GT-55BQQC8';
+    document.head.appendChild(gtagScript);
+    
+    // Initialize dataLayer and gtag function
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag; // Make gtag globally available
+    
+    // Configure GA4
+    gtag('js', new Date());
+    gtag('config', 'GT-55BQQC8', {
+      'send_page_view': true,
+      'cookie_flags': 'SameSite=None;Secure'
+    });
+    
+    console.log('Google Analytics 4 initialized');
+  }
+})();
+
 	showWhiteScreenLoader();
 
 // ===============================
